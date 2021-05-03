171008f1ae2b5136a3776739b4dc7e783e5f860e
David <fernandesc.david@gmail.com>
2021-05-03 06:26:19 -0300

    add welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,48 +1,129 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from 'react';
+import {
+    View,
+    StyleSheet,
+    SafeAreaView,
+    Text,
+    TextInput,
+    Image,
+    KeyboardAvoidingView,
+    Platform,
+    TouchableWithoutFeedback,
+    Keyboard
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+
+import { Button } from '../components/Button';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
+import logo from '../assets/logo.png';
 
 export function Welcome() {
+    const [isFocused, setIsFocused] = useState(false);
+    const [isFilled, setIsFilled] = useState(false);
+    const [name, setName] = useState<string>();
+
     const navigation = useNavigation();
+
+    function handleInputFocus() {
+        setIsFocused(true);
+        setIsFilled(!!name);
+    }
+
+    function handleInputBlur() {
+        setIsFocused(false);
+    }
+
+    function handleInputChange(value: string) {
+        setIsFilled(!!value);
+        setName(value);
+    }
 
     function handleSubmit() {
         navigation.navigate('MyHabits');
     }
 
     return (
-        <View style={styles.container}>
-            <TouchableOpacity style={styles.button}>
-                <Text
-                    style={styles.buttonText}
-                    onPress={handleSubmit}
-                >
-                    Prosseguir
-                </Text>
-            </TouchableOpacity>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <KeyboardAvoidingView
+                style={styles.container}
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            >
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.content}>
+                        <View style={styles.form}>
+                            <View style={styles.header}>
+                                <Image style={styles.logo} source={logo} />
+                                <Text style={styles.title}>
+                                    como você deseja ser chamado?
+                        </Text>
+                            </View>
+
+                            <TextInput
+                                style={[
+                                    styles.input,
+                                    (isFocused || isFilled) && { borderColor: colors.blue }
+                                ]}
+                                placeholder="Digite o nome"
+                                onFocus={handleInputFocus}
+                                onBlur={handleInputBlur}
+                                onChangeText={handleInputChange}
+                            />
+                            <View style={styles.footer}>
+                                <Button title="começar" onPress={handleSubmit} />
+                            </View>
+                        </View>
+                    </View>
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        width: '100%',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'space-around'
     },
-    button: {
-        width: 150,
-        height: 45,
-        backgroundColor: colors.blue,
-        borderRadius: 10,
+    content: {
+        flex: 1,
+        width: '100%'
+    },
+    form: {
+        flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        paddingHorizontal: 50
     },
-    buttonText: {
-        color: colors.white,
-        fontFamily: fonts.contentBold,
-        fontSize: 16
+    header: {
+        alignItems: 'center'
+    },
+    logo: {
+        width: 100,
+        height: 60,
+        marginBottom: 30
+    },
+    title: {
+        fontFamily: fonts.title,
+        fontSize: 28,
+        color: colors.textDark,
+        textAlign: 'center'
+    },
+    input: {
+        borderBottomWidth: 1,
+        borderColor: colors.textUnfocus,
+        color: colors.textDark,
+        width: '100%',
+        fontSize: 16,
+        marginTop: 50,
+        padding: 10,
+        textAlign: 'center'
+    },
+    footer: {
+        width: '100%',
+        marginTop: 30
     }
 })
